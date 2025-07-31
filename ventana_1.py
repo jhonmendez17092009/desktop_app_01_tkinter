14b5c236c03e8d7065c54539be228f9edfb41169
jhonmendez17092009 <jhoasnelmendez@colegioguanenta.edu.co>
2025-07-31 15:47:08 -0500

    push

---
+++ ventana_1.py
@@ -0,0 +1,23 @@
+# se importa la libreria tkinter con todas sus funciones 
+from tkinter import *
+
+#-------------------------------
+# funcionessde la app
+#-------------------------------
+
+
+#-------------------------------
+# ventana principal de la app
+#-------------------------------
+
+# se declara una variable llamada ventana_principal, que adquiere las caracteristica de un objeto de tipo tk()
+ventana_principal = Tk()
+
+# titulo de la ventana 
+ventana_principal.title("Jhoasnel Jose Mendez Aguilar")
+
+# tamaño de la ventana
+ventana_principal.geometry("800x500")
+
+# metodo principal que despliega la ventana en pantalla
+ventana_principal.mainloop()
