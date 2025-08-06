5cb4afed55f53d34ef24534b5542a495ce319958
jhonmendez17092009 <jhoasnelmendez@colegioguanenta.edu.co>
2025-08-06 08:16:23 -0500

    push

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 aplicacion de escritorio  usando la libreria tkinter de Python
 ![imagen](img/screen02.jpg "imagen")
 
+![imagen](img/jh.jpg "imagen")
--- ventana_1.py
+++ ventana_1.py
@@ -1,11 +1,20 @@
 
 from tkinter import *
-
+from tkinter import messagebox
 # -----------------------------------------------------
 #funciones de la app
 #------------------------------------------------------
-
-
+def sumar():
+    c = int(a.get()) + int(b.get())
+    t_resultados.insert(INSERT,"la suma de " + a.get() + " + " + b.get() + "casi siempre es " + str(c) + "\n") 
+def borrar():
+    messagebox.showinfo("suma 1.0", "los datos seran borrados...")
+    a.set("")
+    b.set("")
+    t_resultados.delete("1.0", "end")
+def salir():
+    messagebox.showinfo("suma 1.0", "la app se cerrara...")
+    ventana_principal.destroy()
 #------------------------------------------------------
 # ventana principal
 #------------------------------------------------------
@@ -73,7 +82,7 @@ label_b.place(x = 585 , y = 120)
 
 # entry para el segundo valor 
     
-entry_b = Entry(frame_1, width=4 , textvariable = a)
+entry_b = Entry(frame_1, width=4 , textvariable = b)
 entry_b.config(font=("Arial", 20), justify=CENTER)
 entry_b.focus_set()
 entry_b.place(x = 682 , y = 120)
@@ -87,17 +96,17 @@ frame_2.config(bg="ivory2", width=780,height=120)
 frame_2.place(x=10,y=260)
 # boton para sumar
 img_bt_sumar = PhotoImage(file="img/boton_sumar.png")
-bt_sumar = Button(frame_2, image = img_bt_sumar, width = 105, height = 105)
+bt_sumar = Button(frame_2, image = img_bt_sumar, width = 105, height = 105, command=sumar)
 # bt_sumar=Button(frame_2, text="Sumar", width=10)
 bt_sumar.place(x =166, y = 7)
 # boton para borrar entrada y resultados
 img_bt_borrar = PhotoImage(file="img/boton_borrar.png")
-bt_borrar = Button(frame_2, image = img_bt_borrar, width = 105, height = 105)
+bt_borrar = Button(frame_2, image = img_bt_borrar, width = 105, height = 105, command=borrar)
 # bt_sumar=Button(frame_2, text="Sumar", width=10)
 bt_borrar.place(x =337, y = 7)
 # boton para cerrar la app
 img_bt_salir = PhotoImage(file="img/boton_salir.png")
-bt_salir = Button(frame_2, image = img_bt_salir, width = 105, height = 105)
+bt_salir = Button(frame_2, image = img_bt_salir, width = 105, height = 105, command=salir)
 # bt_sumar=Button(frame_2, text="Sumar", width=10)
 bt_salir.place(x = 558, y = 7)
 
